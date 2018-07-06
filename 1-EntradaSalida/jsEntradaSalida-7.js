17223b9e89005a0e288ff275720cb9d8ba87d2ae
Spaiser <ale.disanso@gmail.com>
2018-07-05 22:18:40 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,59 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+
+	numerouno = document.getElementById('numeroUno').value;
+	numerouno = parseInt(numerouno);
+	numerodos = document.getElementById('numeroDos').value;
+	numerodos = parseInt(numerodos);
+	resultado = numerouno+numerodos;
+	alert ("la suma es " + resultado);
 }
 
 function restar()
 {
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+
+	numerouno = document.getElementById('numeroUno').value;
+	numerouno = parseInt(numerouno);
+	numerodos = document.getElementById('numeroDos').value;
+	numerodos = parseInt(numerodos);
+	resultado = numerouno-numerodos;
+	alert ("la resta es " + resultado);
 }
 
 function multiplicar()
 { 
+	var numerouno;
+	var numerodos;
+	var resultado;
 	
+	numerouno = document.getElementById('numeroUno').value;
+	numerouno = parseInt(numerouno);
+	numerodos = document.getElementById('numeroDos').value;
+	numerodos = parseInt(numerodos);
+	resultado = numerouno*numerodos;
+	alert ("la multiplicacion da " + resultado)
 }
 
 function dividir()
 {
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+	var resto;
+
+	numerouno = document.getElementById('numeroUno').value;
+	numerouno = parseInt(numerouno);
+	numerodos = document.getElementById('numeroDos').value;
+	numerodos = parseInt(numerodos);
+	resultado = numerouno/numerodos;
+	resto = numerouno%numerodos;
+	alert ("la division da " + resultado + (" con resto de ") + resto);
 }
 
